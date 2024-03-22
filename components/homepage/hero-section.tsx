4a3754116c1723d089ad2bf317d7d1e994968369
CJskii <czareczarek3@gmail.com>
2024-03-22 13:51:34 +0000

    Updating logos on the landing page + TODOS

--- components/homepage/hero-section.tsx
+++ components/homepage/hero-section.tsx
@@ -1,7 +1,7 @@
 import ellipse1 from "@/assets/ellipse1.svg";
 import ellipse2 from "@/assets/ellipse2.svg";
 import layerzero from "@/assets/layerzero-logo.svg";
-import wormhole from "@/assets/warmhole-logo.svg";
+import hyperlane from "@/assets/hyperlane-logo.svg";
 import { ChevronRightIcon } from "lucide-react";
 import Image from "next/image";
 import Marquee from "react-fast-marquee";
@@ -42,9 +42,20 @@ export default function HeroSection() {
       <div className=" w-auto md:mb-10">
         <Marquee autoFill pauseOnClick>
           <div className="bg-background-secondary flex w-full items-center py-5">
-            <Image alt="layerzero-logo" src={layerzero} className="mx-6" />
-            {/* // TODO: Replace wormhole with the Hyperlane logo */}
-            {/* <Image alt="wormhole-logo" src={wormhole} className="mx-6" /> */}
+            <Image
+              alt="layerzero-logo"
+              src={layerzero}
+              className="mx-6"
+              width={160}
+              height={42}
+            />
+            <Image
+              alt="wormhole-logo"
+              src={hyperlane}
+              className="mx-6"
+              width={160}
+              height={42}
+            />
           </div>
         </Marquee>
       </div>
