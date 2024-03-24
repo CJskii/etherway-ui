2c810038d6193c6c390220ace2c812ecea7f2c07
CJskii <czareczarek3@gmail.com>
2024-03-24 13:32:53 +0000

    fix prod build + install sharp

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -52,11 +52,12 @@ function MyApp({ Component, pageProps }: AppProps<{ session: Session }>) {
   return (
     <>
       <Script
+        id="google-analytics"
         strategy="lazyOnload"
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
       />
 
-      <Script strategy="lazyOnload">
+      <Script strategy="lazyOnload" id="google-analytics-init">
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
