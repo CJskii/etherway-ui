ab5bfe16e9d78d1c19afd6d8b2f9b1a26e125cbf
Dhruv-2003 <dhruvagarwal2017@gmail.com>
2024-04-22 17:29:54 +0530

    feat: Squid Router Iframe added and tested

---
+++ pages/bridge.tsx
@@ -0,0 +1,23 @@
+import NFTBridge from "@/components/nft/bridge";
+import { Layout } from "@/components/dashboard/layout";
+import HeadComponent from "@/common/elements/HeadComponent";
+import { SquidRouterIFrame } from "@/common/utils/squid";
+
+const ONFTBridge = () => {
+  const params = {
+    contractProvider: { type: "layerzero", contract: "ONFT" },
+    stepDescription: "Bridge ONFT",
+  };
+
+  return (
+    <Layout className="px-0 pt-24 pb-8 min-h-[90vh]">
+      <HeadComponent
+        title="Etherway: ONFT Bridge - Cross-Chain Transactions with ONFTs"
+        description=" Seamlessly bridge ONFTs across multiple chains with Etherway's ONFT Bridge. Discover the power of cross-chain transactions and unlock the potential of decentralized future."
+      />
+      <SquidRouterIFrame />
+    </Layout>
+  );
+};
+
+export default ONFTBridge;
--- pages/index.tsx
+++ pages/index.tsx
@@ -13,22 +13,13 @@ import HeadComponent from "../common/elements/HeadComponent";
 import { Container } from "@/components/ui/Container";
 import { Footer } from "@/components/ui/footer";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
-import { SquidWidget } from "@0xsquid/widget";
-import { AppConfig } from "@0xsquid/widget/widget/core/types/config";
+import { SquidRouterIFrame } from "@/common/utils/squid";
+
 // import SquidRouter from "@/common/utils/squid";
 
 // TODO: Review all meta tags and SEO content
 
 const Home: NextPage = () => {
-  const config = {
-    companyName: "Etherway",
-    integratorId: "etherway-2c794744-6972-4f23-bdcb-784032b1a377",
-    slippage: 1,
-    instantExec: true,
-    infiniteApproval: false,
-    apiUrl: "https://api.squidrouter.com",
-  } as AppConfig;
-
   return (
     <>
       <HeadComponent />
