c31ccce0efffbb8961acaa569425eed2a241a1ce
CJskii <czareczarek3@gmail.com>
2024-05-03 13:45:02 +0100

    ui: mock out bridge ui

---
+++ components/squid/bridge.tsx
@@ -0,0 +1,220 @@
+import { Button } from "@/components/ui/button";
+import { useAccount } from "wagmi";
+import { RouteRequest } from "@0xsquid/squid-types";
+import {
+  RouteType,
+  executeSquidRoute,
+  getSquidChains,
+  getSquidRoute,
+  getSquidTokens,
+} from "@/common/utils/squid/squidRouter";
+import { useState } from "react";
+import getProviderOrSigner from "@/common/utils/getters/getProviderOrSigner";
+import { Signer } from "ethers";
+import { Typography } from "../ui/typography";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+import SquidNetworkModal from "./network-modal";
+import { Network } from "@/common/types/network";
+
+interface NetworkModalProps {
+  props: {
+    selectedNetwork: Network;
+    onNetworkSelect: (network: Network) => void;
+    filteredChains: Network[];
+    dialogTitle: string;
+    dialogDescription: string;
+    commandHeading: string;
+  };
+}
+
+export const SquidBridge = () => {
+  const { address } = useAccount();
+  const [route, setRoute] = useState<RouteType | undefined>();
+
+  const handleBridgeButton = () => {
+    console.log("Bridge button clicked");
+  };
+
+  const handlePreviewButton = () => {
+    console.log("Preview button clicked");
+  };
+
+  const routeParams: RouteRequest = {
+    fromChain: "43114", // Avalanche
+    fromAmount: "10000000000000000", // 0.1 AVAX
+    fromToken: "0xEEeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
+    toChain: "137", // Polygon
+    toToken: "0xEEeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
+    fromAddress: address,
+    toAddress: address,
+    slippageConfig: {
+      autoMode: 1,
+    },
+  };
+
+  const TestingButtons = () => {
+    return (
+      <div className="flex gap-4">
+        <Button
+          variant={"etherway"}
+          onClick={async () => {
+            const _route = await getSquidRoute(routeParams);
+            if (_route) {
+              setRoute(_route);
+            }
+          }}
+        >
+          Get Squid Route
+        </Button>
+        <Button
+          variant={"etherway"}
+          onClick={async () => {
+            if (route) {
+              const signer = (await getProviderOrSigner(true)) as Signer;
+              const _route = await executeSquidRoute(route, signer);
+            }
+          }}
+        >
+          Execute
+        </Button>
+        <br />
+
+        <Button
+          variant={"etherway"}
+          onClick={async () => {
+            console.log(await getSquidTokens());
+            console.log(await getSquidChains());
+          }}
+        >
+          Get
+        </Button>
+      </div>
+    );
+  };
+
+  const fromBridgeProps = {
+    selectedNetwork: {
+      name: "Avalanche",
+
+      nativeCurrency: {
+        name: "AVAX",
+        symbol: "AVAX",
+      },
+    },
+    onNetworkSelect: (network: Network) => {
+      console.log(network);
+    },
+    filteredChains: [
+      {
+        name: "Avalanche",
+
+        nativeCurrency: {
+          name: "AVAX",
+          symbol: "AVAX",
+        },
+      },
+      {
+        name: "Ethereum",
+
+        nativeCurrency: {
+          name: "ETH",
+          symbol: "ETH",
+        },
+      },
+    ],
+    dialogTitle: "Select a network to bridge from",
+    dialogDescription: "Select a network to bridge from",
+    commandHeading: "Select a network",
+  };
+
+  const toBridgeProps = {
+    selectedNetwork: {
+      name: "Polygon",
+
+      nativeCurrency: {
+        name: "MATIC",
+        symbol: "MATIC",
+      },
+    },
+    onNetworkSelect: (network: Network) => {
+      console.log(network);
+    },
+    filteredChains: [
+      {
+        name: "Polygon",
+
+        nativeCurrency: {
+          name: "MATIC",
+          symbol: "MATIC",
+        },
+      },
+      {
+        name: "Ethereum",
+
+        nativeCurrency: {
+          name: "ETH",
+          symbol: "ETH",
+        },
+      },
+    ],
+    dialogTitle: "Select a network to bridge to",
+    dialogDescription: "Select a network to bridge to",
+    commandHeading: "Select a network",
+  };
+
+  return (
+    <div className=" z-10 py-20 md:py-16 flex items-center justify-center flex-col min-h-[90vh]">
+      <TestingButtons />
+      <div className="bg-gradient my-auto md:rounded-xl md:w-7/12 lg:w-5/12 w-full items-start">
+        <div className="p-8 md:p-14 md:px-16 flex flex-col gap-6">
+          <Typography variant={"h3"} className=" dark:text-black text-center">
+            Squid Bridge
+          </Typography>
+
+          {/* <BridgeModal
+            props={{
+              isOpen: showBridgingModal,
+              setIsOpen: setShowBridgingModal,
+              isLoading: isLoading,
+              errorMessage: errorMessage,
+              setErrorMessage: setErrorMessage,
+              nftId: nftId,
+              errorHeader: "There was an error while bridging your NFT",
+              successHeader: "NFT Sent",
+              loadingHeader: "We're working hard to bridge your NFT",
+            }}
+          /> */}
+
+          <div className="flex flex-col">
+            <Label className=" space-y-2">
+              <Typography variant={"large"} className="dark:text-black">
+                Bridge From
+              </Typography>
+            </Label>
+
+            <SquidNetworkModal props={fromBridgeProps} />
+          </div>
+
+          <div className="flex flex-col">
+            <Label className=" space-y-2 w-full ">
+              <Typography variant={"large"} className="dark:text-black">
+                Bridge To
+              </Typography>
+            </Label>
+            <SquidNetworkModal props={toBridgeProps} />
+          </div>
+
+          <Label className=" space-y-2">
+            <Typography variant={"large"} className="dark:text-black">
+              Some label
+            </Typography>
+          </Label>
+          <Button className=" py-6 w-full dark:bg-black dark:text-white dark:hover:bg-black/80 rounded-xl">
+            Bridge
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ components/squid/network-modal.tsx
@@ -0,0 +1,130 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogClose,
+} from "@/components/ui/dialog";
+
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+} from "@/components/ui/command";
+import { Typography } from "../ui/typography";
+import { CardContent } from "../ui/card";
+
+import { Network } from "@/common/types/network";
+import Image from "next/image";
+
+interface NetworkModalProps {
+  props: {
+    selectedNetwork: any; // TODO: Update this type to SQUID
+    onNetworkSelect: (network: Network) => void;
+    filteredChains: any[]; // TODO: Update this type to SQUID
+    dialogTitle: string;
+    dialogDescription: string;
+    commandHeading: string;
+  };
+}
+
+const SquidNetworkModal = ({ props }: NetworkModalProps) => {
+  const {
+    selectedNetwork,
+    onNetworkSelect,
+    filteredChains,
+    dialogTitle,
+    dialogDescription,
+    commandHeading,
+  } = props;
+
+  const handleSelection = (network: Network) => {
+    onNetworkSelect(network);
+  };
+
+  return (
+    <Dialog>
+      <DialogTrigger>
+        <CardContent className="grid gap-4 px-0 pb-0 min-w-full">
+          <div className="flex items-center space-x-4 bg-white/30 rounded-md border border-black p-4 overflow-hidden">
+            <Image
+              src={selectedNetwork.iconUrl as string}
+              width={40}
+              height={40}
+              alt={selectedNetwork.name}
+            />
+            <div className="flex-1 space-y-1 text-start">
+              <Typography
+                variant={"smallTitle"}
+                className="dark:text-black font-semibold truncate"
+              >
+                {selectedNetwork.name}
+              </Typography>
+              <p className="text-sm text-muted-foreground dark:text-black">
+                {selectedNetwork.nativeCurrency.symbol}
+              </p>
+            </div>
+          </div>
+        </CardContent>
+      </DialogTrigger>
+      <DialogContent className="rounded-xl bg-gradient p-6 border-0">
+        <DialogHeader>
+          <DialogTitle>{dialogTitle}</DialogTitle>
+          {/* <DialogDescription>{dialogDescription}</DialogDescription> */}
+        </DialogHeader>
+        <Command>
+          <div></div>
+          <CommandInput placeholder="Search for a network..." />
+          <DialogClose>
+            <CommandList>
+              <CommandEmpty>No results found.</CommandEmpty>
+              <CommandGroup
+                // heading={commandHeading}
+                className="text-white text-left mt-4"
+              >
+                {filteredChains.map((network) => (
+                  <CommandItem
+                    key={network.name}
+                    onSelect={() => handleSelection(network)}
+                    className="flex items-center p-4 mb-1 rounded-md bg-white/30 cursor-pointer justify-start  gap-2 hover:opacity-80"
+                  >
+                    <Image
+                      src={network.iconUrl as string}
+                      width={30}
+                      height={30}
+                      alt="network icon"
+                      className="rounded-full"
+                    />
+                    <div className="flex flex-col text-lg">
+                      <Typography
+                        variant={"smallTitle"}
+                        className="dark:text-black"
+                      >
+                        {network.name}
+                      </Typography>
+                      <Typography
+                        variant={"extraSmall"}
+                        className="dark:text-black text-muted-foreground"
+                      >
+                        {network.nativeCurrency.symbol}
+                      </Typography>
+                    </div>
+                  </CommandItem>
+                ))}
+              </CommandGroup>
+              <CommandSeparator />
+            </CommandList>
+          </DialogClose>
+        </Command>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default SquidNetworkModal;
